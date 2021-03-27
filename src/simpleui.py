1cc9e04088d91eb54d4e4448cadb8bb2e7821655
AMWDragon <79026544+AMWDragon@users.noreply.github.com>
2021-03-27 14:40:46 -0500

    Created a simple UI class

---
+++ src/simpleui.py
@@ -0,0 +1,8 @@
+from PySide2 import QtWidgets
+
+
+class SimpleUI (QtWidgets.QDialog):
+
+    def __init__(self):
+        super(SimpleUI, self).__init__()
+        self.setWindowTitle("A Simple UI")
