add3119f71f021b09fbeb70528cea3cd5f28334b
AMWDragon <79026544+AMWDragon@users.noreply.github.com>
2021-04-10 14:55:39 -0500

    Set up plan for code

---
+++ src/scatter.py
@@ -0,0 +1,16 @@
+import maya.cmds as cmds
+import maya.OpenMaya as om
+
+# determine what the user has selected.
+
+# Store all available objects in a list(drop down menu)and change the selection
+
+# Store all available objects in a list to move objects to it's vertices
+
+# create instances of the object
+
+# move instances to polygon vertices
+
+# change transform node scale randomly for instances
+
+# change transform node rotation randomly for instances
